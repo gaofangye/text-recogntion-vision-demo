1b97221d296d8713e57a28d10d5d000cc6f492a7
1033727528@qq.com <1033727528@qq.com>
2023-09-13 09:15:39 +0800

    Initial Commit

---
+++ text-recogntion-vision-demo/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  text-recogntion-vision-demo
+//
+//  Created by nannan on 2023/9/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ text-recogntion-vision-demo/text_recogntion_vision_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  text_recogntion_vision_demoApp.swift
+//  text-recogntion-vision-demo
+//
+//  Created by nannan on 2023/9/13.
+//
+
+import SwiftUI
+
+@main
+struct text_recogntion_vision_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
