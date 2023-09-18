6e0f781ea3d770cc0fad18de28983f47a210a285
1033727528@qq.com <1033727528@qq.com>
2023-09-18 14:42:33 +0800

    fix: 解决因 iphone 手机拍摄导致图片逆时针旋转90度，无法准确定位文本绘制边框的问题。

--- text-recogntion-vision-demo/ContentView.swift
+++ text-recogntion-vision-demo/ContentView.swift
@@ -46,7 +46,8 @@ struct ContentView: View {
                 self.isImagePickerPresented = true
             }
             .sheet(isPresented: $isImagePickerPresented, onDismiss: {
-                if let image = self.selectedImage {
+                if var image = self.selectedImage {
+                    image = image.rotate(radians: 0)!
                     self.processImage(image: image)
                 }
             }) {
@@ -116,6 +117,31 @@ struct ContentView: View {
         let visionTextInfosData = try? JSONEncoder().encode(visionTextInfos)
         let visionTextInfosJson = String(bytes: visionTextInfosData!, encoding: .utf8)
         print(visionTextInfosJson!.description)
+    }
+}
+
+extension UIImage {
+    // 重新绘制图片并旋转
+    func rotate(radians: Float) -> UIImage? {
+        var newSize = CGRect(origin: CGPoint.zero, size: self.size).applying(CGAffineTransform(rotationAngle: CGFloat(radians))).size
+        // Trim off the extremely small float value to prevent core graphics from rounding it up
+        newSize.width = floor(newSize.width)
+        newSize.height = floor(newSize.height)
+        
+        UIGraphicsBeginImageContextWithOptions(newSize, false, self.scale)
+        guard let context = UIGraphicsGetCurrentContext() else { return nil }
+        
+        // Move origin to middle
+        context.translateBy(x: newSize.width/2, y: newSize.height/2)
+        // Rotate around middle
+        context.rotate(by: CGFloat(radians))
+        // Draw the image at its center
+        self.draw(in: CGRect(x: -self.size.width/2, y: -self.size.height/2, width: self.size.width, height: self.size.height))
+        
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return newImage
     }
 }
 
